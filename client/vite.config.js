2ad27c617c4b092ac443e62c6d75302bd09938e1
CalRya <lindsaysalvacion110@gmail.com>
2025-03-14 23:07:01 +0800

    reloadanoda

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Try '/' if './' causes issues
+  base: '/', // Ensures proper routing on Vercel
 });
